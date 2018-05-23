43c1f5e012f1df21032cebb095512bfc297a4b46
TabJs <cheng_xiaona@126.com>
2018-05-23 23:48:35 +0800

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,10 @@
 const htmlPlugin = require('html-webpack-plugin');
 const extractPlugin = require('extract-text-webpack-plugin');
+const optimizeCss = require('optimize-css-assets-webpack-plugin');
 const path = require('path');
 
 module.exports = {
+  mode: 'development',
   entry: [
     './src/tab.js',
     './src/example.js'
@@ -36,13 +38,19 @@ module.exports = {
         collapseWhitespace: true
       }
     }),
-    new extractPlugin('./src/example.css')
+    new extractPlugin('./src/example.css'),
+    new optimizeCss({
+      assetNameRegExp: /\.optimize\.css$/g,
+      cssProcessor: require('cssnano'),
+      cssProcessorOptions: { discardComments: { removeAll: true } },
+      canPrint: true
+    })
   ],
+  optimization: {     minimize: true,     minimizer: [new optimizeCss({})]   } ,
   devServer: {
-  	contentBase: path.join(__dirname, "example"),
-  	  compress: true,
-  	  port: 9000,
-  	  open: true
-
+    contentBase: path.join(__dirname, "example"),
+    compress: true,
+    port: 9000,
+    open: true
   }
 };
